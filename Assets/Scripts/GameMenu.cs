17b3db732545c0730d9d3f1d34fcefca94fc9883
Kyle Phan <kyledphan@gmail.com>
2023-04-25 23:20:53 -0400

    Add game modes, rules screen

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -8,13 +8,23 @@ public class GameMenu : MonoBehaviour
 {
     public GameObject menu;
     public Button PauseButton;
+    public GameObject RulesScreen;
 
     public void ResetGame() {
-        SceneManager.LoadScene("GameScene");
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
     }
 
     public void MainMenu() {
         SceneManager.LoadScene("LobbyScene");
+    }
+
+    public void Rules() {
+        RulesScreen.SetActive(true);
+    }
+
+    public void Back() {
+        RulesScreen.SetActive(false);
     }
 
     public void Continue() {
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,12 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject SelectGame;
+    public Button Dusk;
+    public Button Day;
+    public Button Night;
+    public Button BackButton;
+
+    void Start() {
+        SelectGame.SetActive(false);
+    }
+
     public void StartGame() {
-        SceneManager.LoadScene("GameScene");
+        SelectGame.SetActive(true);
+    }
+
+    public void DuskGame() {
+        SceneManager.LoadScene("DuskBowling");
+    }
+
+    public void DayGame() {
+        SceneManager.LoadScene("DayBowling");
+    }
+
+    public void NightGame() {
+        SceneManager.LoadScene("NightBowling");
+    }
+
+    public void Back() {
+        SelectGame.SetActive(false);
     }
 
     void Update() {
